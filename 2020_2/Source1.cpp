b67111251b8a8d57e02e80d4a44431d7e393caa0
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-03-11 10:19:01 +0100

    a

--- 2020_2/Source1.cpp
+++ 2020_2/Source1.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main() {
+void sym(int a, int b) {
+	if (a != 0) {
+		sym(a – 1, b + 1);
+		std::cout << a * b;
+		sym(a – 1, b + 1);
+	}
+}
 
+
+int main() {
+	sym(3, 1);
 	return 0;
 }
